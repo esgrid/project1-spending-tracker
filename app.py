205e54556395f6a3b63416dd482b018109f75771
Esgrid <esikahall@gmail.com>
2021-12-14 17:24:10 +0000

    sorted the initial budget assignment, modifying app.py and index.html of the homepage. Added bits to merchnants' and tags' index pages

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from controllers.tag_controller import tags_blueprint
 from controllers.transaction_controller import transactions_blueprint
 from controllers.budget_controller import budget_blueprint
 
+import repositories.budget_repository as budget_repository
+
 app = Flask(__name__)
 
 app.register_blueprint(budget_blueprint)
@@ -14,7 +16,12 @@ app.register_blueprint(transactions_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    try:
+        budget = budget_repository.select_all()[0]
+    except:
+        budget = ''
+
+    return render_template('index.html', budget = budget)
 
 if __name__ == '__main__':
     app.run(debug = True)
