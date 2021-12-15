bd19fb7986c9e600e8fdc3dc46e7fcf9e0f3c097
Esgrid <esikahall@gmail.com>
2021-12-15 14:30:18 +0000

    tags templates and tags controller sorted

--- controllers/tag_controller.py
+++ controllers/tag_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, render_template, request, redirect
 import repositories.tag_repository as tag_repository
+from models.tags import Tag
 
 tags_blueprint = Blueprint("tags", __name__)
 
@@ -15,3 +16,10 @@ def show_tag(id):
     tag = tag_repository.select(id)
     transactions = tag_repository.transactions(tag)
     return render_template('tags/show.html', tag = tag, transactions = transactions)
+
+@tags_blueprint.route('/tags', methods = ['POST'])
+def create_tag():
+    form = request.form
+    active = True if 'active' in form else False
+    tag_repository.save(Tag(form['name'], active))
+    return redirect('/tags')
